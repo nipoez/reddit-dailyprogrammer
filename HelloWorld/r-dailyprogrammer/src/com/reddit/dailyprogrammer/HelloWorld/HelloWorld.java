477b607d53bcd5938f9e0d9842bb3a31e6dd52f0
nipoez <github.joi@harmoney.us>
2012-10-25 07:38:49 -0600

    Initial HelloWorld commit.

---
+++ HelloWorld/r-dailyprogrammer/src/com/reddit/dailyprogrammer/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Hello World Git experiment
+ */
+package com.reddit.dailyprogrammer.HelloWorld;
+
+/**
+ * @author nipoez
+ * 
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
