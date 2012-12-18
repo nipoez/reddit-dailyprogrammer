249cb36c18dd40fa35ca3ed79f54899b491e7818
nipoez <github.joi@harmoney.us>
2012-12-18 08:12:46 -0700

    Functional for "puma"

--- 114_E_Word_ladder_steps/src/com/reddit/dailyprogrammer/nipoez/dp114e/WordLadder.java
+++ 114_E_Word_ladder_steps/src/com/reddit/dailyprogrammer/nipoez/dp114e/WordLadder.java
@@ -1,13 +1,41 @@
 package com.reddit.dailyprogrammer.nipoez.dp114e;
 
-public class WordLadder {
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
+
+public class WordLadder {
+private static String wordsFilePath = System.getProperty("user.dir")
++ "\\selected_four-letter_words.txt";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Useful things
-		System.out.println("Hello World");
+		Set<String> allWords = new HashSet<String>();
+		try {
+
+			// Use Scanner for a first pass of tokenization,
+			// based on whitespace
+			Scanner tokenize = new Scanner(new File(wordsFilePath));
+			while (tokenize.hasNext()) {
+				// Use StringTokenizer for a second pass of tokenization,
+				// treating certain punctuation characters as both
+				// delimiters and tokens
+				allWords.add(tokenize.next().toLowerCase());
+			}
+			tokenize.close();
+		} catch (Exception e) {// Poor practice exception handling
+			System.err.println("Error: " + e.getMessage());
+		}
+		String ladderStart = "puma";
+		for (String word : allWords){
+			if(StringUtils.getLevenshteinDistance(ladderStart, word, 1) > 0){
+				System.out.println(word);
+			}
+		}
 	}
 
 }
