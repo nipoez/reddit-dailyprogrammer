82ef88c78c28f4149b53904dcfb91f4919636459
nipoez <github.joi@harmoney.us>
2012-12-18 07:43:14 -0700

    Hello World baseline.

---
+++ 114_E_Word_ladder_steps/src/com/reddit/dailyprogrammer/nipoez/dp114e/WordLadder.java
@@ -0,0 +1,13 @@
+package com.reddit.dailyprogrammer.nipoez.dp114e;
+
+public class WordLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Useful things
+		System.out.println("Hello World");
+	}
+
+}
