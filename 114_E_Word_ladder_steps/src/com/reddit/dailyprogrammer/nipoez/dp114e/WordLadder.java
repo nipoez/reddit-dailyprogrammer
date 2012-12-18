124d43feeae6edce148166a02e33523590bfb342
nipoez <github.joi@harmoney.us>
2012-12-18 09:17:53 -0700

    Bonus 2 completed. Added caching for performance.

--- 114_E_Word_ladder_steps/src/com/reddit/dailyprogrammer/nipoez/dp114e/WordLadder.java
+++ 114_E_Word_ladder_steps/src/com/reddit/dailyprogrammer/nipoez/dp114e/WordLadder.java
@@ -2,16 +2,17 @@ package com.reddit.dailyprogrammer.nipoez.dp114e;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class WordLadder {
-	private static String wordsFilePath = System.getProperty("user.dir")
-			+ "\\selected_four-letter_words.txt";
+	private final static String WORDS_FILE_PATH = System
+			.getProperty("user.dir") + "\\selected_four-letter_words.txt";
+	private static Map<String, List<String>> neighborsCache = new HashMap<String, List<String>>();
 
 	/**
 	 * @param args
@@ -22,7 +23,7 @@ public class WordLadder {
 
 			// Use Scanner for a first pass of tokenization,
 			// based on whitespace
-			Scanner tokenize = new Scanner(new File(wordsFilePath));
+			Scanner tokenize = new Scanner(new File(WORDS_FILE_PATH));
 			while (tokenize.hasNext()) {
 				// Use StringTokenizer for a second pass of tokenization,
 				// treating certain punctuation characters as both
@@ -52,14 +53,54 @@ public class WordLadder {
 						+ bonusOneCount + " neighbors.");
 			}
 		}
+		System.out
+				.println("------- Bonus 2: Within 3 steps of \"best\" -------");
+		String bonusTwoWord = "best";
+		List<String> bonusTwoStepOne = getNeighbors(bonusTwoWord, allWords);
+		List<String> bonusTwoStepTwo = new ArrayList<String>();
+		List<String> bonusTwoStepThree = new ArrayList<String>();
+		List<String> bonusTwoAll = new ArrayList<String>(bonusTwoStepOne);
+		List<String> bonusTwoTmp = null;
+		for (String step2Word : bonusTwoStepOne) {
+			bonusTwoTmp = getNeighbors(step2Word, allWords, bonusTwoAll);
+			bonusTwoStepTwo.addAll(bonusTwoTmp);
+			bonusTwoAll.addAll(bonusTwoTmp);
+		}
+		for (String step3Word : bonusTwoStepTwo) {
+			bonusTwoTmp = getNeighbors(step3Word, allWords, bonusTwoAll);
+			bonusTwoStepThree.addAll(bonusTwoTmp);
+			bonusTwoAll.addAll(bonusTwoTmp);
+		}
+
+		System.out.println("\"best\" has " + bonusTwoAll.size());
+
 	}
 
 	private static List<String> getNeighbors(final String word,
 			final List<String> allWords) {
-		List<String> neighbors = new ArrayList<String>();
-		for (String potentialNeighbor : allWords) {
-			if (StringUtils.getLevenshteinDistance(word, potentialNeighbor, 1) > 0) {
-				neighbors.add(potentialNeighbor);
+		List<String> neighbors = null;
+		if (neighborsCache.containsKey(word)) {
+			neighbors = neighborsCache.get(word);
+		} else {
+			neighbors = new ArrayList<String>();
+			for (String potentialNeighbor : allWords) {
+				if (StringUtils.getLevenshteinDistance(word, potentialNeighbor,
+						1) > 0) {
+					neighbors.add(potentialNeighbor);
+				}
+			}
+			neighborsCache.put(word, neighbors);
+		}
+		return neighbors;
+	}
+
+	private static List<String> getNeighbors(final String word,
+			final List<String> allWords, final List<String> excludedWords) {
+		List<String> potentialNeighbors = getNeighbors(word, allWords);
+		List<String> neighbors = new ArrayList<String>(potentialNeighbors);
+		for (String potentialNeighbor : potentialNeighbors) {
+			if (excludedWords.contains(potentialNeighbor)) {
+				neighbors.remove(potentialNeighbor);
 			}
 		}
 		return neighbors;
